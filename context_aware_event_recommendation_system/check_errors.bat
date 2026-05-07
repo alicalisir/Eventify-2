13ad6eb751042d1484d3e318b4d61b8e867d07cf
Anil Demir <anill.demir34@gmail.com>
2026-05-07 23:37:54 +0300

    Gereksiz md dosyalı silindi

--- context_aware_event_recommendation_system/check_errors.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "c:\Users\Huawei\Desktop\Klasorlerim\Projelerim\context_aware_event_recommendation_system\context_aware_event_recommendation_system"
-echo Checking for errors...
-flutter analyze
-pause
